1d2c6175e7f0bb854ac032f44e78e4230fc56596
Daniel Rosa Franzini <daniel.franzini@gmail.com>
2021-04-27 18:29:34 -0300

    Fixed (again) configuration of intl build break.

--- scripts/001-binutils.sh
+++ scripts/001-binutils.sh
@@ -37,7 +37,7 @@ for TARGET in "dvp"; do
 	rm -rf build-$TARGET && mkdir build-$TARGET && cd build-$TARGET || { exit 1; }
 
         ## first, configure intl
-        ../intl/configure --disable-build-warnings --prefix="$PS2DEV/$TARGET" --target="$TARGET" $TARG_XTRA_OPTS
+        ../intl/configure --disable-build-warnings --prefix="$PS2DEV/$TARGET" --target="$TARGET" $TARG_XTRA_OPTS || { exit 1; }
 
 	## Configure the build.
 	../configure --disable-build-warnings --prefix="$PS2DEV/$TARGET" --target="$TARGET" $TARG_XTRA_OPTS || { exit 1; }
